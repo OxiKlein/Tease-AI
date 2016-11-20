1fd1672868d6571f4cbf272f3a6062bc7174d067
Stefaf <stefaf@users.noreply.github.com>
2016-11-20 01:36:35 +0100

    Qnd - Startup logging This should show you where the exception on startup occurs.

--- Tease AI/Form6.vb
+++ Tease AI/Form6.vb
@@ -1,3 +1,5 @@
 ﻿Public Class FrmSplash
-
+	Private Sub LBLSplash_TextChanged(sender As Object, e As EventArgs) Handles LBLSplash.TextChanged
+		Trace.WriteLine("SpashText: " & LBLSplash.Text)
+	End Sub
 End Class
